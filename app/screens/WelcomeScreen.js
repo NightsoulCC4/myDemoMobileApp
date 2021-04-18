0222b973205431138979ff4e1ed9c96f5450f7bf
NightsoulCC4 <StickyCoffee8360@gmail.com>
2021-04-18 18:23:28 +0700

    add prototype

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Text, Image, ImageBackground, StyleSheet, View } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/tenka.jpg")} />
+        <Text>Sell What you don't need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "purple",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "violet",
+  },
+});
+
+export default WelcomeScreen;
